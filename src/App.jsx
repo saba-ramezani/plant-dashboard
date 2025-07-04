b33733ba97f1eb46d177bdbef6e8b0e33e2cb0bb
saba-ramezani <saba.rad.sr@gmail.com>
2025-07-04 14:05:31 +0330

    initial layout

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { Button, Layout } from 'antd';
+import {MenuUnfoldOutlined, MenuFoldOutlined} from '@ant-design/icons'
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import 'antd/dist/reset.css';
+import { useState } from 'react';
+import './App.css'
+import Sidebar from './components/Sidebar/Sidebar';
+
+const { Header, Sider, Content } = Layout;
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false)
+  return (
+    <Layout>
+      <Sider
+        theme='light'
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        className='sider'
+      >
+        <Sidebar />
+        <Button
+          type='text'
+          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          onClick={() => setCollapsed(!collapsed)}
+          className='collapse-menu-btn'
+          />
+      </Sider>
+      <Layout>
+        <Header className='header'>
+          <div>xxxxxx</div>
+        </Header>
+        <Content className='content'>qqqqqqq</Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default App;
