8761514227d51e0ebeefe4e5ffed73944d90a5b4
saba-ramezani <saba.rad.sr@gmail.com>
2025-07-05 21:52:29 +0330

    made contentSidebar fully responsive

--- src/components/ContentSidebar/ContentSidebar.jsx
+++ src/components/ContentSidebar/ContentSidebar.jsx
@@ -1,37 +1,48 @@
-import { Card, Flex, Image, Typography } from "antd"
-import { plant1 } from "../../assets"
+import { Card, Flex, Image, Typography } from "antd";
+import { plant1 } from "../../assets";
 
 const ContentSidebar = ({ isMobile }) => {
   return (
     <div>
       <Card className="card" style={{ position: 'relative', overflow: 'hidden', height: '350px' }}>
-        <Flex vertical gap="large" justify="center">
-          <Typography.Title level={4} strong>
-            Today <br /> 5 Orders
-          </Typography.Title>
-          <Typography.Title level={4} strong>
-            This Month <br /> 240 Orders
-          </Typography.Title>
-        </Flex>
+        
+        <Flex style={{ height: '100%' }} justify="space-between" align="center">
+          
+          <Flex vertical gap={60} justify="center" align="center" style={{ width: '50%' }}>
+            <div>
+              <Typography.Title level={5}>
+                Today
+              </Typography.Title>
+              <Typography.Title level={3} strong>
+                5 Orders
+              </Typography.Title>
+            </div>
 
-        {/* Responsive plant image */}
-        {!isMobile && (
-          <Image
-            src={plant1}
-            alt="plant"
-            style={{
-              position: 'absolute',
-              bottom: -100,
-              left: '50px',
-              height: '300px',
-              width: 'auto',
-            }}
-            preview={false}
-          />
-        )}
+            <div className="">
+              <Typography.Title level={5}>
+                This Month
+              </Typography.Title>
+              <Typography.Title level={3} strong>
+                240 Orders
+              </Typography.Title>
+            </div>
+          </Flex>
+
+          <Flex justify="center" align="center" style={{ width: '50%' }}>
+            <Image
+              src={plant1}
+              alt="plant"
+              style={{
+                height: '300px',
+                width: 'auto',
+              }}
+              preview={false}
+            />
+          </Flex>
+        </Flex>
       </Card>
     </div>
   );
 };
 
-export default ContentSidebar
+export default ContentSidebar;
